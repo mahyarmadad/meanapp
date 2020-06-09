2b40c53e6ab4b41c92c6b2172bbc78742bcb2e60
mahyarmadad <[Your>
2020-06-09 15:32:21 +0430

    Finish Backend

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { MynameComponent } from './myname/myname.component';
 import { FormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [AppComponent, MynameComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = procces.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+
 const connectDB = require("./database/connect");
 connectDB();
 
