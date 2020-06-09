3fe4f70dcd4387004a9ceff10ae728eb14dcbacd
mahyarmadad <[Your>
2020-06-09 15:16:59 +0430

    Finish Backend

---
+++ app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = procces.env.PORT || 3000;
+const connectDB = require("./database/connect");
+connectDB();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "Frontend/dist")));
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/tasks", require("./routes/tasks"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "Frontend/dist/index.html"));
+});
+app.listen(port, () => console.log(`Example app listening on port port!`));
---
+++ database/connect.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGOURI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+module.exports = connectDB;
